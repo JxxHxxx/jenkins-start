13d0665988cdf464d8faecb56d2c206e5943cc4f
JxxHxxx <leesin5498@naver.com>
2023-05-08 12:49:41 +0900

    init commit

---
+++ src/main/java/com/jxx/jenkinsstart/JenkinsStartApplication.java
@@ -0,0 +1,13 @@
+package com.jxx.jenkinsstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jxx/jenkinsstart/JenkinsStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxx.jenkinsstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
